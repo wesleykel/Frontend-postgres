3f1033079e814e48e51072aa24a2922a9720829e
Wesley Kelly <wesley.kelly@hotmail.com>
2022-10-20 10:35:38 +0100

    initial commit

---
+++ front_pg/__tests__/index.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen  } from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
+import Home from '../pages/index'
+import '@testing-library/jest-dom'
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+describe('Home', () => {
+  it('renders a login text', () => {
+    render(<UserProvider><Home /></UserProvider>)
+
+    const login = screen.getByText('Login') 
+    expect(login).toBeInTheDocument()
+  })
+
+
+ /* it("once Login is clicked is renders logout",  ()=>{
+
+    render(<UserProvider><Home /></UserProvider>)
+
+    const login = screen.getByText('Login') 
+
+    userEvent.click(login)
+
+    const logout =  screen.getByText("Logout")
+expect(logout).toBeInTheDocument()
+  })
+})*/
+
+
---
+++ front_pg/pages/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next'
+import { useUser } from '@auth0/nextjs-auth0'
+//import Head from 'next/head'
+//import Image from 'next/image'
+//import styles from '../styles/Home.module.css'
+
+const Home: NextPage = (data) => {
+  
+  
+  const {user, error ,isLoading}=useUser()
+  
+  console.log(data)
+  console.log(user?.nickname)
+   return(
+     <>
+     <h1>Chop Shop</h1>
+     <div>
+   <p>
+
+   {!user&&<a href="/api/auth/login">Login</a>}
+  
+
+   </p>
+
+    {user&&<a href="/api/auth/logout">Logout</a>}
+   </div>
+     </>
+   ) 
+  
+}
+
+export async function getServerSideProps(){
+
+  const res = await fetch("http://localhost:3091/user/GSSP")
+  const data = await res.json()
+  
+  return{
+    props:data
+  }
+}
+export default Home
