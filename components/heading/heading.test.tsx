976690e4758fa4ac8ea5badec143e563e818c890
Wesley Kelly <wesley.kelly@hotmail.com>
2022-10-21 16:29:58 +0100

    tailwind library added basic styling added

--- components/heading/heading.test.tsx
+++ components/heading/heading.test.tsx
@@ -4,12 +4,14 @@ import Heading from "./heading"
 
 describe("should render heading", ()=>{
 
-it("should display correct title of ` The Shop`", ()=>{
+it("should display correct title of ` The Community Chest`", ()=>{
 
 render(<Heading/>)
 
-const title = screen.getByText("The Shop")
+const title = screen.getByText("The Community Chest")
+const title2 = screen.getByText("Food Bank and Community Hub")
 expect(title).toBeInTheDocument()
+expect(title2).toBeInTheDocument()
 })
 
 
--- components/heading/heading.tsx
+++ components/heading/heading.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Heading = () => {
     return (
-        <div className=" w-full ">
-         <h1 className=" shadow-inner text-center text-6xl font-mono py-3 border border-grey-600 " >The Shop</h1>   
+        <div className=" w-full shadow-inner  border border-grey-600 py-3 bg-stone-50  ">
+         <h1 className=" text-green-900   text-center text-6xl font-mono py-3 " >The Community Chest</h1> 
+         <h2 className="text-center font-mono">Your Food Bank and Community Hub</h2>  
         </div>
     )
 }
